60a47d9e9a073b354a37141b5cf50ec406d826f9
Werner Dweight <wd@wernerdweight.com>
2017-05-13 11:32:25 +0200

    Added watermarking

--- Image/Image.php
+++ Image/Image.php
@@ -2,7 +2,8 @@
 
 namespace WernerDweight\ImageManager\Image;
 
-Class Image{
+Class Image {
+
 	private $width;
 	private $height;
 	private $ext;
@@ -11,66 +12,80 @@ Class Image{
 	private $encrypted;
 	private $autorotate;
 
-	public function __construct($path = null,$ext = null,$secret = null,$autorotate = false){
-		$this->secret = substr(hash('sha256',($secret ? $secret : 'I did not want to tell you, but this is not secret at all (change this in config)!')),0,32);
+	public function __construct(string $path = null, string $ext = null, string $secret = null, bool $autorotate = false) {
+		$this->secret = substr(hash('sha256', ($secret ? $secret : 'I did not want to tell you, but this is not secret at all (change this in config)!')), 0, 32);
 		$this->autorotate = $autorotate;
-		if($path) $this->load($path);
-		if($ext) $this->ext = $ext;
+		if($path) {
+			$this->load($path);
+		}
+		if($ext) {
+			$this->ext = $ext;
+		}
 	}
 
-	public function create($dimensions){
+	public function create(array $dimensions) : self {
 		$this->width = $dimensions['width'];
 		$this->height = $dimensions['height'];
-		$this->workingData = imagecreatetruecolor($this->width,$this->height);
-		if($this->ext == 'png') $this->setTransparency(false,true);
-		if($this->ext == 'gif') $this->setTransparency();
+		$this->workingData = imagecreatetruecolor($this->width, $this->height);
+		if($this->ext == 'png') {
+			$this->setTransparency(false, true);
+		}
+		if($this->ext == 'gif') {
+			$this->setTransparency();
+		}
 		$this->encrypted = false;
+		return $this;
 	}
 
-	private function autoRotate($path){
+	private function autoRotate(string $path) : self {
 		$exifData = exif_read_data($path);
-		if(true === isset($exifData['Orientation'])){
+		if(true === isset($exifData['Orientation'])) {
 			switch ($exifData['Orientation']) {
 				case 3:
-					$this->workingData = imagerotate($this->workingData,180,0);
+					$this->workingData = imagerotate($this->workingData, 180, 0);
 					break;
 				case 6:
-					$this->workingData = imagerotate($this->workingData,-90,0);
+					$this->workingData = imagerotate($this->workingData, -90, 0);
 					break;
 				case 8:
-					$this->workingData = imagerotate($this->workingData,90,0);
+					$this->workingData = imagerotate($this->workingData, 90, 0);
 					break;
 			}
 		}
+		return $this;
 	}
 
-	private function imagecreatefromjpeg($path){
+	private function imagecreatefromjpeg(string $path) : self {
 		$this->workingData = imagecreatefromjpeg($path);
 		$this->encrypted = false;
-		if(true === $this->autorotate){
+		if(true === $this->autorotate) {
 			$this->autoRotate($path);
 		}
+		return $this;
 	}
 
-	private function imagecreatefrompng($path){
+	private function imagecreatefrompng(string $path) : self {
 		$this->workingData = imagecreatefrompng($path);
-		$this->setTransparency(false,true);
+		$this->setTransparency(false, true);
 		$this->encrypted = false;
+		return $this;
 	}
 
-	private function imagecreatefromgif($path){
+	private function imagecreatefromgif(string $path) : self {
 		$this->workingData = imagecreatefromgif($path);
 		$this->setTransparency();
 		$this->encrypted = false;
+		return $this;
 	}
 
-	private function imagecreatefromwdImage($path){
+	private function imagecreatefromwdImage(string $path) : self {
 		$this->workingData = file_get_contents($path);
 		$this->encrypted = true;
+		return $this;
 	}
 
-	public function load($path){
-		$this->ext = strtolower(substr(strrchr($path,'.'),1));
+	public function load(string $path) : self {
+		$this->ext = strtolower(substr(strrchr($path, '.'), 1));
 
 		try {
 			switch ($this->getType($this->ext)) {
@@ -93,75 +108,86 @@ Class Image{
 			throw $e;
 		}
 
-		if(!$this->encrypted) $this->getDimensions();
+		if(!$this->encrypted) {
+			$this->getDimensions();
+		}
+		return $this;
 	}
 
-	private function getDimensions(){
+	private function getDimensions() : self {
 		$this->width = imagesx($this->workingData);
 		$this->height = imagesy($this->workingData);
+		return $this;
 	}
 
-	public function save($path,$name,$ext,$quality){
+	public function save(string $path, string $name, string $ext = null, int $quality = 100) : bool {
 		if(!is_dir($path)) {
-			mkdir($path,0777,true);
+			mkdir($path, 0777, true);
 		}
-		if($ext === null) $ext = $this->ext;
-		if($this->encrypted){
-			file_put_contents($path.$name.'.wdImage',$this->workingData);
+		if($ext === null) {
+			$ext = $this->ext;
+		}
+		if($this->encrypted) {
+			file_put_contents($path.$name.'.wdImage', $this->workingData);
 			return true;
 		}
-		else if(imagetypes()){
+		else if(imagetypes()) {
 			switch ($ext) {
 				case 'gif':
-					if(IMG_GIF) imagegif($this->workingData,$path.$name.'.'.$ext);
+					if(IMG_GIF) {
+						imagegif($this->workingData, $path.$name.'.'.$ext);
+					}
 					break;
 				case 'jpeg':
 				case 'jpg':
-					if(IMG_JPG) imagejpeg($this->workingData,$path.$name.'.'.$ext,$quality);
+					if(IMG_JPG) {
+						imagejpeg($this->workingData, $path.$name.'.'.$ext, $quality);
+					}
 					break;
 				case 'png':
-					if(IMG_PNG) imagepng($this->workingData,$path.$name.'.'.$ext,round(9 - ((9*$quality)/100)));
+					if(IMG_PNG) {
+						imagepng($this->workingData, $path.$name.'.'.$ext, round(9 - ((9*$quality) / 100)));
+					}
 					break;
 				default:
 					throw new \Exception("Unsupported file type", 1);
 			}
+			return false;
 		}
 	}
 
-	private function getType($ext){
+	private function getType(string $ext) : string {
 		switch ($ext) {
 			case 'jpg':
 			case 'jpeg':
 				return 'jpeg';
-				break;
 			case 'png':
 				return 'png';
-				break;
 			case 'gif':
 				return 'gif';
-				break;
 			case 'wdimage':
 				return 'wdImage';
-				break;
 			default:
 				throw new \Exception("Unsupported file type", 1);
-				break;
 		}
 	}
 
-	public function encrypt(){
-		if($this->encrypted) throw new \Exception("Can't encrypt encrypted image", 1);
+	public function encrypt() : self {
+		if($this->encrypted) {
+			throw new \Exception("Can't encrypt encrypted image", 1);
+		}
 
 		/// use buffer to get image content
 		ob_start();
-		imagejpeg($this->workingData,NULL,100);
+		imagejpeg($this->workingData, NULL, 100);
 		$this->workingData =  ob_get_contents();
 		ob_end_clean();
 
 		$this->workingData = rtrim(
 			mcrypt_encrypt(
 				MCRYPT_RIJNDAEL_128,
-				$this->secret, $this->workingData,
+				$this->secret,
+				$this->workingData,
 				MCRYPT_MODE_ECB,
 				mcrypt_create_iv(
 					mcrypt_get_iv_size(
@@ -170,14 +196,19 @@ Class Image{
 					),
 					MCRYPT_RAND
 				)
-			), "\0"
+			),
+			"\0"
 		);
 
 		$this->encrypted = true;
+
+		return $this;
 	}
 
-	public function decrypt(){
-		if(!$this->encrypted) throw new \Exception("Can't decrypt unencrypted image", 1);
+	public function decrypt() : self {
+		if(!$this->encrypted) {
+			throw new \Exception("Can't decrypt unencrypted image", 1);
+		}
 
 		$this->workingData = rtrim(
 			mcrypt_decrypt(
@@ -192,47 +223,59 @@ Class Image{
 					),
 					MCRYPT_RAND
 				)
-			), "\0"
+			),
+			"\0"
 		);
 		$this->encrypted = false;
 
 		$this->workingData = imagecreatefromstring($this->workingData);
 
-		if(!$this->width || !$this->height) $this->getDimensions();
+		if(!$this->width || !$this->height) {
+			$this->getDimensions();
+		}
+
+		return $this;
 	}
 
-	public function getEncrypted(){
+	public function getEncrypted() : bool {
 		return $this->encrypted;
 	}
 
-	public function destroy(){
+	public function destroy() : self {
 		imagedestroy($this->workingData);
+		return $this;
 	}
 
-	public function getData(){
+	public function getData() {
 		return $this->workingData;
 	}
 
-	public function setData($data){
+	public function setData($data) : self {
 		$this->workingData = $data;
+		return $this;
 	}
 
-	public function getWidth(){
+	public function getWidth() : int {
 		return $this->width;
 	}
 
-	public function getHeight(){
+	public function getHeight() : int {
 		return $this->height;
 	}
 
-	public function getExt(){
+	public function getExt() : string {
 		return $this->ext;
 	}
 
-	private function setTransparency($alphaBlending = null,$saveAlpha = null){
+	private function setTransparency($alphaBlending = null, $saveAlpha = null) : self {
 		imagecolortransparent($this->workingData, imagecolorallocate($this->workingData, 0, 0, 0));
-		if($alphaBlending !== null) imagealphablending($this->workingData, $alphaBlending);
-		if($saveAlpha !== null) imagesavealpha($this->workingData, $saveAlpha);
+		if($alphaBlending !== null) {
+			imagealphablending($this->workingData, $alphaBlending);
+		}
+		if($saveAlpha !== null) {
+			imagesavealpha($this->workingData, $saveAlpha);
+		}
+		return $this;
 	}
 
 }
--- Manager/ImageManager.php
+++ Manager/ImageManager.php
@@ -4,110 +4,241 @@ namespace WernerDweight\ImageManager\Manager;
 
 use WernerDweight\ImageManager\Image\Image;
 
-Class ImageManager{
+Class ImageManager {
+
 	private $image;
 	private $secret;
 	private $autorotate;
 
-	public function __construct($secret = null,$autorotate = false){
-		if($secret) $this->secret = $secret;
+	public function __construct(string $secret = null, bool $autorotate = false) {
+		if($secret) {
+			$this->secret = $secret;
+		}
 		$this->autorotate = $autorotate;
 	}
 
-	public function loadImage($path){
+	public function loadImage(string $path) : self {
 		try {
-			$this->image = new Image($path,null,$this->secret,$this->autorotate);
+			$this->image = new Image($path, null, $this->secret, $this->autorotate);
 		} catch (\Exception $e) {
 			throw $e;
 		}
+		return $this;
 	}
 
-	public function saveImage($path,$name,$ext,$quality = 100){
+	public function saveImage(string $path, string $name, string $ext = null, int $quality = 100) : self {
 		try {
-			$this->image->save($path,$name,$ext,$quality);
+			$this->image->save($path, $name, $ext, $quality);
 		} catch (\Exception $e) {
 			throw $e;
 		}
+		return $this;
 	}
 
-	public function resize($width,$height,$crop = false){
-		if($this->image->getEncrypted()){
-			$this->decrypt();
+	public function resizeImage(Image &$image, int $width, int $height, bool $crop = false) : self {
+		if($image->getEncrypted()) {
+			$this->decryptImage($image);
 			$encrypt = true;
 		}
-		else $encrypt = false;
+		else {
+			$encrypt = false;
+		}
 
-		$dimensions = $this->adjustDimenstions($width,$height,$crop);
-		$tmp = new Image(null,$this->image->getExt(),$this->secret,$this->autorotate);
+		$dimensions = $this->getAdjustedImageDimensions($image, $width, $height, $crop);
+		$tmp = new Image(null, $image->getExt(), $this->secret, $this->autorotate);
 		$tmp->create($dimensions);
-		imagecopyresampled($tmp->getData(),$this->image->getData(),0,0,0,0,$dimensions['width'],$dimensions['height'],$this->image->getWidth(),$this->image->getHeight());
-		$this->image->destroy();
-		$this->image = $tmp;
-		if($crop) $this->crop($width,$height);
-		
-		if($encrypt){
-			$this->encrypt();
+		imagecopyresampled($tmp->getData(), $image->getData(), 0, 0, 0, 0, $dimensions['width'], $dimensions['height'], $image->getWidth(), $image->getHeight());
+		$image->destroy();
+		$image = $tmp;
+		if($crop) {
+			$this->cropImage($image, $width, $height);
 		}
+		
+		if($encrypt) {
+			$this->encryptImage($image);
+		}
+		return $this;
 	}
 
-	public function crop($width,$height){
-		if($this->image->getEncrypted()){
-			$this->decrypt();
+	public function resize(int $width, int $height, bool $crop = false) : self {
+		return $this->resizeImage($this->image, $width, $height, $crop);
+	}
+
+	public function cropImage(Image &$image, int $width, int $height) : self {
+		if($image->getEncrypted()) {
+			$this->decryptImage($image);
 			$encrypt = true;
 		}
-		else $encrypt = false;
+		else {
+			$encrypt = false;
+		}
 
-		$crop = $this->adjustCrop($width,$height);
-		$centerX = ($this->image->getWidth()/2) - ($crop['width']/2);
-		$centerY = ($this->image->getHeight()/2) - ($crop['height']/2);
-		$tmp = new Image(null,$this->image->getExt(),$this->secret,$this->autorotate);
-		$tmp->create(array('width' => $width,'height' => $height));
-		imagecopyresampled($tmp->getData(),$this->image->getData(),0,0,$centerX,$centerY,$width,$height,$crop['width'],$crop['height']);
-		$this->image->destroy();
-		$this->image = $tmp;
+		$crop = $this->getAdjustedImageCropDimensions($image, $width, $height);
+		$centerX = ($image->getWidth() / 2) - ($crop['width'] / 2);
+		$centerY = ($image->getHeight() / 2) - ($crop['height'] / 2);
+		$tmp = new Image(null, $image->getExt(), $this->secret, $this->autorotate);
+		$tmp->create([
+			'width' => $width,
+			'height' => $height
+		]);
+		imagecopyresampled($tmp->getData(), $image->getData(), 0, 0, $centerX, $centerY, $width, $height, $crop['width'], $crop['height']);
+		$image->destroy();
+		$image = $tmp;
 
-		if($encrypt){
-			$this->encrypt();
+		if($encrypt) {
+			$this->encryptImage($image);
+		}
+		return $this;
+	}
+
+	public function crop(int $width, int $height) : self {
+		return $this->cropImage($this->image, $width, $height);
+	}
+
+	public function encryptImage(Image &$image) : self {
+		$image->encrypt();
+		return $this;
+	}
+
+	public function encrypt() : self {
+		return $this->encryptImage($this->image);
+	}
+
+	public function decryptImage(Image &$image) : self {
+		$image->decrypt();
+		return $this;
+	}
+
+	public function decrypt() : self {
+		return $this->decryptImage($this->image);
+	}
+
+	private function getAdjustedImageCropDimensions(Image &$image, int $width, int $height, bool $relative = false) : array {
+		$w = $image->getWidth() / $width;
+		$h = $image->getHeight() / $height;
+
+		if($w < 1 || $h < 1) {
+			if($w < $h) {
+				return [
+					'width' => $width * $w,
+					'height' => $height * $w
+				];
+			}
+			else {
+				return [
+					'width' => $width * $h,
+					'height' => $height * $h
+				];
+			}
+		}
+		else {
+			if($w < $h) {
+				return [
+					'width' => $width * (true === $relative ? $w : 1),
+					'height' => $height * (true === $relative ? $w : 1)
+				];
+			}
+			else {
+				return [
+					'width' => $width * (true === $relative ? $h : 1),
+					'height' => $height * (true === $relative ? $h : 1)
+				];
+			}
 		}
 	}
 
-	public function encrypt(){
-		$this->image->encrypt();
-	}
-
-	public function decrypt(){
-		$this->image->decrypt();
-	}
-
-	private function adjustCrop($width,$height){
-		$w = $this->image->getWidth()/$width;
-		$h = $this->image->getHeight()/$height;
-		if($w < 1 || $h < 1){
-			if($w < $h) return array('width' => ($width * $w),'height' => ($height * $w));
-			else return array('width' => ($width * $h),'height' => ($height * $h));
+	private function getAdjustedImageDimensions(Image &$image, int $width, int $height, bool $crop = false) : array {
+		if($image->getWidth() / $image->getHeight() > $width / $height) {		/// current is wider than new
+			if($crop) {	/// upscale prevention
+				return [
+					'width' => $this->getImageWidth($image, $height),
+					'height' => $height
+				];
+			}
+			else {
+				return [
+					'width' => $width,
+					'height' => $this->getImageHeight($image, $width)
+				];
+			}
 		}
-		else return array('width' => $width,'height' => $height);
-	}
-
-	private function adjustDimenstions($width,$height,$crop = false){
-		if($this->image->getWidth()/$this->image->getHeight() > $width/$height){		/// current is wider than new
-			if($crop) return array('width' => $this->getWidth($height),'height' => $height);	/// upscale prevention
-			else return array('width' => $width,'height' => $this->getHeight($width));
-		}
-		else if($this->image->getWidth()/$this->image->getHeight() < $width/$height){	/// current is taller than new
-			if($crop) return array('width' => $width,'height' => $this->getHeight($width));		/// upscale prevention
-			else return array('width' => $this->getWidth($height),'height' => $height);
+		else if($image->getWidth() / $image->getHeight() < $width / $height) {	/// current is taller than new
+			if($crop) {	/// upscale prevention
+				return [
+					'width' => $width,
+					'height' => $this->getImageHeight($image, $width)
+				];
+			}
+			else {
+				return [
+					'width' => $this->getImageWidth($image, $height),
+					'height' => $height
+				];
+			}
 		}
 		else{									/// current has same aspect ratio as new
-			return array('width' => $width,'height' => $height);
+			return [
+				'width' => $width,
+				'height' => $height
+			];
 		}
 	}
 
-	private function getWidth($height){
-		return $height * ($this->image->getWidth() / $this->image->getHeight());
+	public function addImageWatermark(Image &$image, array $parameters) : self {
+		$watermark = new Image($parameters['file'], null, $this->secret, $this->autorotate);
+
+		/// temporarily enable alphablending to be able to use 32-bit images
+		imagealphablending($watermark->getData(), true);
+		imagealphablending($image->getData(), true);
+
+		/// determine watermark position from config
+		if(true === isset($parameters['position'])) {
+			$top = intval($parameters['position']['top']) / 100;
+			$left = intval($parameters['position']['left']) / 100;
+		}
+		else {
+			$top = $left = 1;
+		}
+
+		/// determine watermark size from config
+		if(true === isset($parameters['size'])) {
+			if($parameters['size'] === 'cover') {
+				/// cover dimensions are the same as crop dimensions
+				$dimensions = $this->getAdjustedImageCropDimensions($watermark, $image->getWidth(), $image->getHeight(), true);
+				imagecopyresampled($image->getData(), $watermark->getData(), 0, 0, ($watermark->getWidth() - $dimensions['width']) * $left, ($watermark->getHeight() - $dimensions['height']) * $top, $image->getWidth(), $image->getHeight(), $dimensions['width'], $dimensions['height']);
+			}
+			else if($parameters['size'] === 'contain') {
+				$dimensions = $this->getAdjustedImageDimensions($watermark, $image->getWidth(), $image->getHeight());
+				imagecopyresampled($image->getData(), $watermark->getData(), ($image->getWidth() - $dimensions['width']) * $left, ($image->getHeight() - $dimensions['height']) * $top, 0, 0, $dimensions['width'], $dimensions['height'], $watermark->getWidth(), $watermark->getHeight());
+			}
+			else {		/// percentage
+				$dimensions = $this->getAdjustedImageDimensions($watermark, $image->getWidth(), $image->getHeight());
+				$dimensions['width'] *= (intval($parameters['size']) / 100);
+				$dimensions['height'] *= (intval($parameters['size']) / 100);
+				imagecopyresampled($image->getData(), $watermark->getData(), ($image->getWidth() - $dimensions['width']) * $left, ($image->getHeight() - $dimensions['height']) * $top, 0, 0, $dimensions['width'], $dimensions['height'], $watermark->getWidth(), $watermark->getHeight());
+			}
+		}
+		else {
+			imagecopy($image->getData(), $watermark->getData(), ($image->getWidth() - $watermark->getWidth()) * $left, ($image->getHeight() - $watermark->getHeight()) * $top, 0, 0, $watermark->getWidth(), $watermark->getHeight());
+		}
+
+		/// disable alphablending again to be able to save transparency
+		imagealphablending($watermark->getData(), false);
+		imagealphablending($image->getData(), false);
+
+		return $this;
 	}
 
-	private function getHeight($width){
-		return $width * ($this->image->getHeight() / $this->image->getWidth());
+	public function addWatermark(array $parameters) : self {
+		return $this->addImageWatermark($this->image, $parameters);
 	}
+
+	private function getImageWidth(Image &$image, int $height) : int {
+		return intval($height * ($image->getWidth() / $image->getHeight()));
+	}
+
+	private function getImageHeight(Image &$image, int $width) : int {
+		return intval($width * ($this->image->getHeight() / $this->image->getWidth()));
+	}
+
 }
